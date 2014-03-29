24474d664aac12051588bdd22ce17b1e5271cb7f
Hssn <fhssn1@gmail.com>
2014-03-28 23:35:24 -0500

    final example on open.gl/depthstencils in C and C++

---
+++ p6.c
@@ -0,0 +1,403 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <GL/glx.h>
+#define PNG_DEBUG 3
+#include <png.h>
+
+#include <kazmath/kazmath.h>
+#define M_PI 3.14159265358979323846f
+
+#ifndef nullptr
+  #define nullptr NULL
+#endif
+
+
+// example of print_matrix usage:
+//
+// print_matrix(&model.mat[0]);
+//
+void print_matrix(const float * p_matrix)
+{
+  printf("M: ");
+  for (int i = 0; i < 16; i++) {
+    printf("% 0.2f ", p_matrix[i]);
+  }
+  printf("\n");
+}
+
+
+void abort_(const char * s, ...)
+{
+  //va_list args;
+  //va_start(args, s);
+  //vfprintf(stderr, s, args);
+  //fprintf(stderr, "\n");
+  //va_end(args);
+  abort();
+}
+
+void read_png_file(char* file_name, unsigned char ** p_image, int * p_width, int * p_height)
+{
+  int width, height, y;
+  png_byte color_type;
+  png_byte bit_depth;
+
+  png_structp png_ptr;
+  png_infop info_ptr;
+  int number_of_passes;
+  png_bytep * row_pointers;
+  unsigned char * image;
+
+  int row_ptr_size, row_size;//, total_row_size;
+  int total_bytes, running_offset;
+
+  char header[8];    // 8 is the maximum size that can be checked
+
+  /* open file and test for it being a png */
+  FILE *fp = fopen(file_name, "rb");
+  if (!fp)
+    abort_("[read_png_file] File %s could not be opened for reading", file_name);
+  fread(header, 1, 8, fp);
+  if (png_sig_cmp(header, 0, 8))
+    abort_("[read_png_file] File %s is not recognized as a PNG file", file_name);
+
+
+  /* initialize stuff */
+  png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+  if (!png_ptr)
+    abort_("[read_png_file] png_create_read_struct failed");
+
+  info_ptr = png_create_info_struct(png_ptr);
+  if (!info_ptr)
+    abort_("[read_png_file] png_create_info_struct failed");
+
+  if (setjmp(png_jmpbuf(png_ptr)))
+    abort_("[read_png_file] Error during init_io");
+
+  png_init_io(png_ptr, fp);
+  png_set_sig_bytes(png_ptr, 8);
+
+  png_read_info(png_ptr, info_ptr);
+
+  width = png_get_image_width(png_ptr, info_ptr);
+  height = png_get_image_height(png_ptr, info_ptr);
+  color_type = png_get_color_type(png_ptr, info_ptr);
+  bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+  number_of_passes = png_set_interlace_handling(png_ptr);
+  png_read_update_info(png_ptr, info_ptr);
+
+  // read file
+  if (setjmp(png_jmpbuf(png_ptr)))
+    abort_("[read_png_file] Error during read_image");
+
+  // calculate allocation size
+  total_bytes = 0;
+  for (y=0; y<height; y++)
+    total_bytes += png_get_rowbytes(png_ptr,info_ptr);
+
+  // do the allocation
+  image = (unsigned char *) malloc(total_bytes);
+
+  // allocate a pointer array (just to satisfy png_read_image)
+  row_ptr_size = sizeof(png_bytep) * height;
+  row_pointers = (png_bytep*) malloc(row_ptr_size);
+
+  running_offset = 0;
+  for (y=0; y<height; y++) {
+    row_size = png_get_rowbytes(png_ptr,info_ptr);
+    row_pointers[y] = (png_byte*) &image[running_offset];
+    running_offset += row_size;
+  }
+  png_read_image(png_ptr, row_pointers);
+  //printf("total: %d bytes row_ptr, %d bytes image, %d width, %d height\n", row_ptr_size, total_bytes, width, height);
+  free(row_pointers);
+  fclose(fp);
+
+  *p_width = width;
+  *p_height = height;
+  *p_image = image;
+}
+
+
+const GLchar* vertex_src =
+  "#version 150 core\n"\
+  "in vec3 position;"\
+  "in vec3 color;"\
+  "in vec2 texcoord;"\
+  "out vec3 Color;"\
+  "out vec2 Texcoord;"\
+  "uniform mat4 model;"\
+  "uniform mat4 view;"\
+  "uniform mat4 proj;"\
+  "uniform vec3 overrideColor;"\
+  "void main(){"\
+  "Color=overrideColor * color;"\
+  "Texcoord=texcoord;"\
+  "gl_Position=proj * view * model * vec4(position,1.0);"\
+  "}";
+
+const GLchar* fragment_src =
+  "#version 150 core\n"\
+  "in vec3 Color;"\
+  "in vec2 Texcoord;"\
+  "out vec4 outColor;"\
+  "uniform sampler2D texKitten;"\
+  "uniform sampler2D texPuppy;"\
+  "void main(){"\
+  "vec4 colKitten = texture(texKitten, Texcoord);"\
+  "vec4 colPuppy = texture(texPuppy, Texcoord);"\
+  "vec4 texColor = mix(colKitten, colPuppy, 0.5);"\
+  "outColor = vec4(Color, 1.0) * texColor;"\
+  "}";
+
+//GLfloat vertices[] = {
+//  -0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+//   0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+//   0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+//  -0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f
+//};
+GLfloat vertices[] = {
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+
+    -1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+     1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+     1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+     1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+    -1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+    -1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f
+};
+
+GLuint elements[] = {
+  0, 1, 2,
+  2, 3, 0
+};
+
+int main()
+{
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+  GLFWwindow * window = glfwCreateWindow(1280, 720, "OpenGL", nullptr, nullptr);
+  glfwMakeContextCurrent(window);
+
+
+  glewExperimental = GL_TRUE;
+  glewInit();
+
+
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  GLuint ebo;
+  glGenBuffers(1, &ebo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertex_src, NULL);
+  glCompileShader(vertexShader);
+
+  GLint status;
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+  if (status != GL_TRUE) {
+    printf("Error 1 has occurred. Exiting.\n");
+    glfwTerminate();
+    return 0;
+  }
+
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragment_src, NULL);
+  glCompileShader(fragmentShader);
+
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
+  if (status != GL_TRUE) {
+    printf("Error 1 has occurred. Exiting.\n");
+    glfwTerminate();
+    return 0;
+  }
+
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glBindFragDataLocation(shaderProgram, 0, "outColor");
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
+
+  GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+  glEnableVertexAttribArray(posAttrib);
+  glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), 0);
+
+  GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+  glEnableVertexAttribArray(colAttrib);
+  glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(3*sizeof(float)));
+
+  GLint texAttrib = glGetAttribLocation(shaderProgram, "texcoord");
+  glEnableVertexAttribArray(texAttrib);
+  glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 8*sizeof(float), (void*)(6*sizeof(float)));
+
+  GLuint textures[2];
+  glGenTextures(2, textures);
+  int width, height;
+  unsigned char* image;
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, textures[0]);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  read_png_file("sample.png", &image, &width, &height);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+  free(image);
+  glUniform1i(glGetUniformLocation(shaderProgram, "texKitten"), 0);
+
+  glActiveTexture(GL_TEXTURE1);
+  glBindTexture(GL_TEXTURE_2D, textures[1]);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  read_png_file("sample2.png", &image, &width, &height);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+  free(image);
+  glUniform1i(glGetUniformLocation(shaderProgram, "texPuppy"), 1);
+
+  kmMat4 model;
+  kmMat4Identity(&model);
+  kmMat4RotationZ(&model, 90.0f * M_PI / 180.0f);
+  GLint uniModel = glGetUniformLocation(shaderProgram, "model");
+  glUniformMatrix4fv(uniModel, 1, GL_FALSE, &model.mat[0]);
+
+  kmMat4 view;
+  kmVec3 p_eye = { 1.2f, 1.2f, 1.2f };
+  kmVec3 p_ctr = { 0.0f, 0.0f, 0.0f };
+  kmVec3 p_up  = { 0.0f, 0.0f, 1.0f };
+  kmMat4LookAt(&view, &p_eye, &p_ctr, &p_up);
+  GLint uniView = glGetUniformLocation(shaderProgram, "view");
+  glUniformMatrix4fv(uniView, 1, GL_FALSE, &view.mat[0]);
+
+  kmMat4 proj;
+  kmMat4PerspectiveProjection(&proj, 45.0f, 1280.0f / 720.0f, 1.0f, 10.0f);
+  GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
+  glUniformMatrix4fv(uniProj, 1, GL_FALSE, &proj.mat[0]);
+
+  glEnable(GL_DEPTH_TEST);
+  //glEnable(GL_STENCIL_TEST);
+  //glStencilFunc(GL_GEQUAL, 2, 0xFF);
+  //glStencilFunc(GL_ALWAYS, 1, 0xFF);
+  //glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+  //glStencilMask(0xFF);
+  //glColorMask(GL_FALSE, GL_FALSE, GL_FALSE, GL_FALSE);
+  //glDepthMask(GL_FALSE);
+  //glStencilFunc(GL_EQUAL, 1, 0xFF);
+  //glStencilMask(0x00);
+
+  kmMat4 tmp;
+
+  GLint uniColor = glGetUniformLocation(shaderProgram, "overrideColor");
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(window, GL_TRUE);
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    kmMat4Identity(&model);
+    kmMat4RotationZ(&model, (float)clock() / (float)CLOCKS_PER_SEC * 90.0f * M_PI / 180.0f);
+    glUniformMatrix4fv(uniModel, 1, GL_FALSE, &model.mat[0]);
+    //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
+
+    glEnable(GL_STENCIL_TEST);
+
+      // draw floor
+      glStencilFunc(GL_ALWAYS, 1, 0xFF);
+      glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
+      glStencilMask(0xFF);
+      glDepthMask(GL_FALSE);
+      glClear(GL_STENCIL_BUFFER_BIT);
+
+      glDrawArrays(GL_TRIANGLES, 36, 6);
+
+      // draw cube reflection
+      glStencilFunc(GL_EQUAL, 1, 0xFF);
+      glStencilMask(0x00);
+      glDepthMask(GL_TRUE);
+
+      kmMat4Translation(&tmp, 0.0f, 0.0f, -1.0f);
+      kmMat4Multiply(&model, &model, &tmp);
+      kmMat4Scaling(&tmp, 1.0f, 1.0f, -1.0f);
+      kmMat4Multiply(&model, &model, &tmp);
+      glUniformMatrix4fv(uniModel, 1, GL_FALSE, &model.mat[0]);
+      glUniform3f(uniColor, 0.3f, 0.3f, 0.3f);
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+      glUniform3f(uniColor, 1.0f, 1.0f, 1.0f);
+
+    glDisable(GL_STENCIL_TEST);
+
+    glfwSwapBuffers(window);
+  }
+
+  glDeleteTextures(2, textures);
+  glDeleteProgram(shaderProgram);
+  glDeleteShader(fragmentShader);
+  glDeleteShader(vertexShader);
+  glDeleteBuffers(1, &ebo);
+  glDeleteBuffers(1, &vbo);
+  glDeleteVertexArrays(1, &vao);
+  glfwTerminate();
+  return 0;
+}
